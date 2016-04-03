8e01a38c57c95c91835309bcd390ef931bbb5682
yzhou <zhouyang2640@gmail.com>
2016-04-03 18:30:19 -0500

    Using the flask-script

--- hello.py
+++ hello.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from flask import request
+from flask import make_response
+from flask import redirect
+from flask import abort
+from flask_script import Manager
 
 app = Flask(__name__)
+manager = Manager(app)
 
 
 @app.route('/')
@@ -25,6 +30,24 @@ def agent():
     user_agent = request.headers.get('User-Agent')
     return '<p>Your browser is %s</p>' % (user_agent)
 
+# make response by make_response function, and return
+@app.route('/response')
+def responseTest():
+    response = make_response('<h1>Carry a cookie !</h1>')
+    response.set_cookie('answer', '10')
+    return response
+
+
+# redirect
+@app.route('/redirect')
+def redirectTest():
+    return redirect('http://www.google.com')
+# abort
+@app.route('/abort')
+def abort_test():
+    abort(404)
+    return '<h1>Hello !</h1>'
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
